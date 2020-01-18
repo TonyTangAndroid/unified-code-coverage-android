1da9c2a690f4d59694eaaf0f5f18c30d59bb1b3d
TonyTandAndroid <tonytangandroid@gmail.com>
2020-01-18 01:33:37 -0800

    Add PojoMissed.java

--- app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
+++ app/src/main/java/net/rafaeltoledo/coverage/MainActivity.java
@@ -3,30 +3,32 @@ package net.rafaeltoledo.coverage;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView text;
+  private TextView text;
+  private PojoMissed pojoMissed;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(this);
-        findViewById(R.id.hide).setOnClickListener(this);
-        text = findViewById(R.id.text);
+    findViewById(R.id.button).setOnClickListener(this);
+    findViewById(R.id.hide).setOnClickListener(this);
+    text = findViewById(R.id.text);
+  }
+
+  @Override
+  public void onClick(View v) {
+    if (v.getId() == R.id.button) {
+      text.setText("Hello World!");
+    } else {
+      pojoMissed = new PojoMissed();
+      text.setText(pojoMissed.getThing());
+      v.setVisibility(View.GONE);
     }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.button) {
-            text.setText("Hello World!");
-        } else {
-            v.setVisibility(View.GONE);
-        }
-    }
+  }
 }
